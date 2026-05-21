b04126618aa980923d561c691d9a4c9baee6087d
hoainhi212 <nhhn2102@gmail.com>
2026-05-21 13:13:26 +0700

    Add files via upload

---
+++ frotends/src/components/Footer.js
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <div
+      style={{
+        background: "#111",
+        color: "white",
+        fontFamily: "'Smooch Sans', sans-serif",
+        padding: "20px",
+        marginTop: "25px",
+      }}
+    >
+      <p>
+        Blockchain Food Traceability © 2026
+      </p>
+    </div>
+  );
+}
