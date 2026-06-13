09cba9c6b42bc7ffffc70f7a7a9f4c0b728ad877
vanly0707 <lii.ttr1107@gmail.com>
2026-06-13 13:28:02 +0700

    Update project

--- frotends/src/components/ConnectWallet.jsx
+++ frotends/src/components/ConnectWallet.jsx
@@ -1,50 +1,61 @@
 import {useWeb3} from "../context/Web3Context";
-
+import { useNavigate } from "react-router-dom";
 export default function ConnectWallet() {
 
-  const {
-    connectWallet,
-    address,
-    balance,
-    role
-  } = useWeb3();
 
-  const shortAddress = (addr) => {
-    return `${addr.slice(0,6)}...${addr.slice(-4)}`;
+  const {connectWallet} = useWeb3();
+
+
+  const navigate = useNavigate();
+
+
+  const handleConnect = async () => {
+
+
+    const role =
+      await connectWallet();
+
+
+    if (!role) return;
+
+
+    if (role === "ADMIN") {
+
+
+      navigate("/admin");
+
+
+    }
+    else if (
+      role === "FARMER"
+    ) {
+
+
+      navigate("/farmer");
+
+
+    }
+    else if (
+      role === "INSPECTOR"
+    ) {
+      navigate("/inspector");
+    }
+    else {
+      alert("Ví này không có quyền quản lý hệ thống");
+    }
   };
+
 
   return (
 
-    <div>
 
-      {!address ? (
+    <button
+      onClick={handleConnect}
+    >
+      Connec
+    </button>
 
-        <button
-          onClick={connectWallet}
-        >
-          Connect Wallet
-        </button>
 
-      ) : (
-
-        <div>
-
-          <p>
-            {shortAddress(address)}
-          </p>
-
-          <p>
-            {balance} ETH
-          </p>
-
-          <p>
-            {role}
-          </p>
-
-        </div>
-
-      )}
-
-    </div>
   );
 }
+
--- frotends/src/context/Web3Context.jsx
+++ frotends/src/context/Web3Context.jsx
@@ -4,40 +4,57 @@ import {
   useState
 } from "react";
 
+
 import { ethers } from "ethers";
+
 
 import CONTRACT_ABI
 from "../abi/FoodTrace.json";
 
+
 const Web3Context = createContext();
 
+
 const CONTRACT_ADDRESS =
-process.env.REACT_APP_CONTRACT_ADDRESS;
+import.meta.env.VITE_CONTRACT_ADDRESS;
+
 
 export const Web3Provider = ({
   children
 }) => {
 
+
   const [address, setAddress] =
     useState("");
+
 
   const [balance, setBalance] =
     useState("");
 
+
   const [role, setRole] =
     useState("");
+
+
+  const [signer, setSigner] =
+    useState(null);
+
 
   const [contract, setContract] =
     useState(null);
 
+
   const connectWallet = async () => {
+
 
     try {
 
+
       if (!window.ethereum) {
-        alert("Cài MetaMask");
+        alert("Vui lòng cài MetaMask");
         return;
       }
+
 
       // provider
       const provider =
@@ -45,19 +62,23 @@ export const Web3Provider = ({
           window.ethereum
         );
 
+
       // mở metamask
       await provider.send(
         "eth_requestAccounts",
         []
       );
 
+
       // signer
       const signer =
         await provider.getSigner();
 
+
       // address
       const walletAddress =
         await signer.getAddress();
+
 
       // contract
       const contract =
@@ -67,97 +88,75 @@ export const Web3Provider = ({
           signer
         );
 
+
       // balance
       const balanceWei =
         await provider.getBalance(
           walletAddress
         );
 
+
       const balanceEth =
         ethers.formatEther(balanceWei);
 
-      // role constants
-      const FARMER_ROLE =
-        ethers.keccak256(
-          ethers.toUtf8Bytes(
-            "FARMER_ROLE"
-          )
-        );
 
-      const INSPECTOR_ROLE =
-        ethers.keccak256(
-          ethers.toUtf8Bytes(
-            "INSPECTOR_ROLE"
-          )
-        );
-
-      // check roles
-      const isFarmer =
-        await contract.hasRole(
-          FARMER_ROLE,
+      // Lấy role từ constants
+      const userRole =
+        await contract.getMyRole(
           walletAddress
         );
 
-      const isInspector =
-        await contract.hasRole(
-          INSPECTOR_ROLE,
-          walletAddress
-        );
-
-      const owner =
-        await contract.owner();
-
-      // xác định role
-      if (
-        owner.toLowerCase()
-        ===
-        walletAddress.toLowerCase()
-      ) {
-        setRole("admin");
-      }
-      else if (isInspector) {
-        setRole("inspector");
-      }
-      else if (isFarmer) {
-        setRole("farmer");
-      }
-      else {
-        setRole("guest");
-      }
 
       setAddress(walletAddress);
+
 
       setBalance(
         Number(balanceEth).toFixed(4)
       );
 
+
+      setRole(userRole);
+
+
+      setSigner(signer);
+
+
       setContract(contract);
+
+
+      return userRole;
+
 
     } catch (error) {
 
-      console.log(error);
 
+      console.error(error);
+
+
+      return null;
     }
   };
 
-  return (
 
+  return (
     <Web3Context.Provider
       value={{
         address,
         balance,
         role,
+        signer,
         contract,
         connectWallet
       }}
     >
-
       {children}
-
     </Web3Context.Provider>
-
   );
 };
 
+
 export const useWeb3 = () =>
   useContext(Web3Context);
+
+
+
